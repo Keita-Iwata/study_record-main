86b0a20236ca72aaf31337366e5df8dfbe853338
Keita-Iwata <gantarooou.market@gmail.com>
2024-06-27 10:21:20 +0900

    23th

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Create a single supabase client for interacting with your database
+console.log(process.env.VITE_SUPABASE_URL);
 const SUPABASE_URL = process.env.VITE_SUPABASE_URL as string;
 const SUPABASE_ANON_KEY = process.env.VITE_SUPABASE_ANON_KEY as string;
 
