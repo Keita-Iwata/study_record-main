5a64c4f1ed92a189e0aa8b91e0599e71e67102bb
岩田　圭太 <gantarooou.market@gmail.com>
2024-06-28 20:48:39 +0900

    26th

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 
 console.log("supabaseURL");
 console.log(process.env.VITE_SUPABASE_URL);
-const SUPABASE_URL = process.env.VITE_SUPABASE_URL as string;
-const SUPABASE_ANON_KEY = process.env.VITE_SUPABASE_ANON_KEY as string;
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
