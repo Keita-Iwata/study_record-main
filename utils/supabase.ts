ed4ffed81096a1cdc98bb934126fd1456952d096
岩田　圭太 <gantarooou.market@gmail.com>
2024-06-29 15:38:05 +0900

    32th

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,10 +1,18 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Create a single supabase client for interacting with your database
+// Ensure that the environment variables are correctly accessed
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log("supabaseURL");
-console.log(import.meta.env.VITE_SUPABASE_URL);
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL as string;
-const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+if (!SUPABASE_URL) {
+  throw new Error('supabaseUrl is required.');
+}
+
+if (!SUPABASE_ANON_KEY) {
+  throw new Error('supabaseAnonKey is required.');
+}
+
+console.log("Supabase URL: ", SUPABASE_URL);
+console.log("Supabase Anon Key: ", SUPABASE_ANON_KEY);
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
