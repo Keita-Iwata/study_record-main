46499b26d54a23617b674f2dc6bedfd0da1e7553
岩田　圭太 <gantarooou.market@gmail.com>
2024-07-01 06:11:57 +0900

    41th

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import env from "vite-plugin-env-compatible";
 
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env.VITE_SUPABASE_URL': JSON.stringify(process.env.VITE_SUPABASE_URL),
-    'process.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(process.env.VITE_SUPABASE_ANON_KEY),
-  },
+    plugins: [react(), env({ prefix: "VITE", mountedPath: "process.env" })],
 });
