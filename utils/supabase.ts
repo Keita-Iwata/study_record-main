69f05e139cce18e616bb925c2e95b499759bf3af
岩田　圭太 <gantarooou.market@gmail.com>
2024-07-01 05:43:32 +0900

    34th

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -5,13 +5,15 @@ console.log('Environment Variables:', process.env);
 const SUPABASE_URL = process.env.VITE_SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.VITE_SUPABASE_ANON_KEY;
 
-// 環境変数の存在確認
-if (!SUPABASE_URL) {
-  throw new Error('supabaseUrl is required.');
-}
-
+console.log("supabaseAnonKey");
 if (!SUPABASE_ANON_KEY) {
   throw new Error('supabaseAnonKey is required.');
+}
+
+// 環境変数の存在確認
+console.log("supabaseURL");
+if (!SUPABASE_URL) {
+  throw new Error('supabaseUrl is required.');
 }
 
 console.log("Supabase URL: ", SUPABASE_URL);
